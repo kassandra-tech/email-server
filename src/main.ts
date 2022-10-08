44cd719a150226e973c19d4815e4d9f3b0c0125e
Oleksii Kucheruk <oleksii.kucheruk+github@teamdev.com>
2022-10-08 09:07:15 +0300

    Replace `.env` file with Heroku environment variable

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
-
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-
-require('dotenv').config();
-
-const { Port } = process.env;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
@@ -18,8 +13,8 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, options);
   
     SwaggerModule.setup('api', app, document);
-  
-    await app.listen(Port);
+
+    await app.listen(process.env.PORT);
   
     console.log(`App is running on: ${await app.getUrl()}`);
   }
